d7cf9fa4885c35d329ae2077f82ff2b3a11f6fe0
Atzin Escandia <escandiatech@gmail.com>
2025-01-08 14:09:20 +0100

    Navbar spacing

--- components/ui/NavBar.tsx
+++ components/ui/NavBar.tsx
@@ -29,7 +29,7 @@ const NavBar = (): JSX.Element => {
         backgroundColor: "rgba(255, 255, 255, 0.1)",
         backdropFilter: "blur(10px)",
       }}
-      className="sticky top-0 left-0 h-screen shadow-lg backdrop-blur-3xl border-r-2 border-white/10 w-16 flex flex-col z-50 justify-evenly"
+      className="sticky top-0 left-0 h-screen shadow-lg backdrop-blur-3xl border-r-2 border-white/10 w-10 sm:w-16 flex flex-col z-50 justify-evenly"
     >
       {navItems.map(({ name, link }, index) => {
         const isActive = `#${activeSection}` === link;
@@ -47,7 +47,7 @@ const NavBar = (): JSX.Element => {
               className={cn(
                 "text-sm font-extrabold cursor-pointer",
                 isActive
-                  ? "bg-slate-900/[0.8] h-full text-white font-extrabold border-t border-white p-5 items-center flex"
+                  ? "bg-slate-900/[0.8] h-full text-white font-extrabold border-t border-white p-2 sm:p-5 items-center flex"
                   : "opacity-50 hover:opacity-100"
               )}
             >
